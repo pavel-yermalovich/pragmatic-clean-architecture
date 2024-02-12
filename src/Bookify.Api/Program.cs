194914853b5850119061aaccaefdddf50a4aa001
Pavel Yermalovich <x48ray@gmail.com>
2024-02-12 18:28:00 +0100

    Added Serilog

--- src/Bookify.Api/Program.cs
+++ src/Bookify.Api/Program.cs
@@ -1,8 +1,12 @@
 using Bookify.Api.Extensions;
 using Bookify.Application;
 using Bookify.Infrastructure;
+using Serilog;
 
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Host.UseSerilog((context, configuration) =>
+    configuration.ReadFrom.Configuration(context.Configuration));
 
 builder.Services.AddControllers();
 
@@ -26,6 +30,10 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+app.UseRequestContextLogging();
+
+app.UseSerilogRequestLogging();
 
 app.UseCustomExceptionHandler();
 
--- src/Bookify.Application/Abstractions/Behaviors/LoggingBehavior.cs
+++ src/Bookify.Application/Abstractions/Behaviors/LoggingBehavior.cs
@@ -1,16 +1,19 @@
 ﻿using Bookify.Application.Abstractions.Messaging;
+using Bookify.Domain.Abstractions;
 using MediatR;
 using Microsoft.Extensions.Logging;
+using Serilog.Context;
 
 namespace Bookify.Application.Abstractions.Behaviors;
 
 public class LoggingBehavior<TRequest, TResponse>
     : IPipelineBehavior<TRequest, TResponse>
     where TRequest : IBaseCommand
+    where TResponse : Result
 {
-    private readonly ILogger<TRequest> _logger;
+    private readonly ILogger<LoggingBehavior<TRequest, TResponse>> _logger;
 
-    public LoggingBehavior(ILogger<TRequest> logger)
+    public LoggingBehavior(ILogger<LoggingBehavior<TRequest, TResponse>> logger)
     {
         _logger = logger;
     }
@@ -24,17 +27,27 @@ public class LoggingBehavior<TRequest, TResponse>
 
         try
         {
-            _logger.LogInformation("Executing command {Command}", name);
+            _logger.LogInformation("Executing request {Request}", name);
 
             var result = await next();
 
-            _logger.LogInformation("Command {Command} processed successfully", name);
+            if (result.IsSuccess)
+            {
+				_logger.LogInformation("Request {Request} processed successfully", name);
+			}
+            else
+            {
+				using (LogContext.PushProperty("Error", result.Error, true))
+                {
+					_logger.LogError("Request {Request} processed with error", name);
+				}
+			}
 
             return result;
         }
         catch (Exception exception)
         {
-            _logger.LogError(exception, "Command {Command} processing failed", name);
+            _logger.LogError(exception, "Request {Request} processing failed", name);
 
             throw;
         }
