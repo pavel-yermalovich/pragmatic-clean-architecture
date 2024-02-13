1363a565d93a7ce7a3e84021306803afc72c727d
Pavel Yermalovich <x48ray@gmail.com>
2024-02-13 14:15:49 +0100

    Added health checks

--- src/Bookify.Api/Program.cs
+++ src/Bookify.Api/Program.cs
@@ -1,6 +1,8 @@
 using Bookify.Api.Extensions;
 using Bookify.Application;
 using Bookify.Infrastructure;
+using HealthChecks.UI.Client;
+using Microsoft.AspNetCore.Diagnostics.HealthChecks;
 using Serilog;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -42,5 +44,10 @@ app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
+
+app.MapHealthChecks("health", new HealthCheckOptions
+{
+    ResponseWriter = UIResponseWriter.WriteHealthCheckUIResponse
+});
 
 app.Run();
