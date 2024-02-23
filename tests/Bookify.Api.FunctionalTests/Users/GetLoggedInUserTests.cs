165000f12576a60c85366874c363a6511eb978a9
Pavel Yermalovich <x48ray@gmail.com>
2024-02-23 13:21:49 +0700

    Added functional tests for logging in and getting logged in user

---
+++ tests/Bookify.Api.FunctionalTests/Users/GetLoggedInUserTests.cs
@@ -0,0 +1,42 @@
+﻿using Bookify.Api.FunctionalTests.Infrastructure;
+using Bookify.Application.Users.GetLoggedInUser;
+using FluentAssertions;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using System.Net;
+using System.Net.Http.Headers;
+using System.Net.Http.Json;
+
+namespace Bookify.Api.FunctionalTests.Users;
+
+public class GetLoggedInUserTests : BaseFunctionalTest
+{
+	public GetLoggedInUserTests(FunctionalTestWebAppFactory factory) : base(factory)
+	{
+	}
+
+	[Fact]
+	public async Task Get_ShouldReturnUnauthorized_WhenAccessTokenIsMissing()
+	{
+		// Act
+		var response = await HttpClient.GetAsync("/api/v1/users/me");
+
+		// Assert
+		response.StatusCode.Should().Be(HttpStatusCode.Unauthorized);
+	}
+
+	[Fact]
+	public async Task Get_ShouldReturnUser_WhenAccessTokenIsNotMissing() 
+	{
+		// Arrange
+		var accessToken = await GetAccessToken();
+		HttpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue(
+			JwtBearerDefaults.AuthenticationScheme, 
+			accessToken);
+
+		// Act
+		var user = await HttpClient.GetFromJsonAsync<UserResponse>("/api/v1/users/me");
+
+		// Assert
+		user.Should().NotBeNull();
+	}
+}
